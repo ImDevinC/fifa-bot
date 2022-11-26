690be7c5d2656dac3eb850f497461d7cb0cdeaec
Devin Collins <me@imdevinc.com>
2022-11-26 12:23:03 -0800

    test: remove tracing test

--- pkg/app/matches_test.go
+++ pkg/app/matches_test.go
@@ -2,15 +2,12 @@ package app_test
 
 import (
 	"context"
-	"errors"
 	"testing"
-	"time"
 
 	go_fifa "github.com/ImDevinC/go-fifa"
 	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
 	"github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
 	"github.com/aws/aws-sdk-go-v2/service/sqs"
-	"github.com/getsentry/sentry-go"
 	"github.com/imdevinc/fifa-bot/pkg/app"
 	"github.com/imdevinc/fifa-bot/pkg/database"
 	"github.com/imdevinc/fifa-bot/pkg/queue"
@@ -59,22 +56,22 @@ func TestMatches(t *testing.T) {
 	}
 }
 
-func TestTracing(t *testing.T) {
-	err := sentry.Init(sentry.ClientOptions{
-		Dsn:              "https://500ec0d1c287437da8165c0a4833054f@o4504167699382272.ingest.sentry.io/4504169589309440",
-		Debug:            true,
-		TracesSampleRate: 1.0,
-		Release:          "development",
-	})
-	if err != nil {
-		t.Error(err)
-	}
-	ctx := context.Background()
-	defer sentry.Flush(2 * time.Second)
-	span := sentry.StartSpan(ctx, "function", sentry.TransactionName("TestTracing"))
-	defer span.Finish()
-	childspan := sentry.StartSpan(span.Context(), "function")
-	defer childspan.Finish()
-	err = errors.New("this is an error")
-	sentry.CaptureException(err)
-}
+// func TestTracing(t *testing.T) {
+// 	err := sentry.Init(sentry.ClientOptions{
+// 		Dsn:              "https://500ec0d1c287437da8165c0a4833054f@o4504167699382272.ingest.sentry.io/4504169589309440",
+// 		Debug:            true,
+// 		TracesSampleRate: 1.0,
+// 		Release:          "development",
+// 	})
+// 	if err != nil {
+// 		t.Error(err)
+// 	}
+// 	ctx := context.Background()
+// 	defer sentry.Flush(2 * time.Second)
+// 	span := sentry.StartSpan(ctx, "function", sentry.TransactionName("TestTracing"))
+// 	defer span.Finish()
+// 	childspan := sentry.StartSpan(span.Context(), "function")
+// 	defer childspan.Finish()
+// 	err = errors.New("this is an error")
+// 	sentry.CaptureException(err)
+// }

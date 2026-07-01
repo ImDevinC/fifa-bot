45d2ede0665f41f37788a65bb7900061c1ed6958
Devin Collins <3997333+ImDevinC@users.noreply.github.com>
2026-07-01 11:11:43 -0700

    feat: enable subs

--- pkg/fifa/fifa_test.go
+++ pkg/fifa/fifa_test.go
@@ -5,30 +5,35 @@ import (
 	"testing"
 
 	"github.com/imdevinc/fifa-bot/pkg/fifa"
+	"github.com/imdevinc/fifa-bot/pkg/models"
 	go_fifa "github.com/imdevinc/go-fifa"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestLiveEvents(t *testing.T) {
 	client := go_fifa.Client{}
-	matchResp, err := fifa.GetLiveMatches(context.Background(), &client)
+	//	matchResp, err := fifa.GetLiveMatches(context.Background(), &client)
+	//	if ok := assert.NoError(t, err); !ok {
+	//		t.FailNow()
+	//	}
+	//	if ok := assert.Greater(t, len(matchResp), 0); !ok {
+	//		t.FailNow()
+	//	}
+	m := models.Match{
+		CompetitionId: "17",
+		SeasonId:      "285023",
+		StageId:       "289287",
+		MatchId:       "400021512",
+	}
+	resp, err := fifa.GetMatchEvents(context.Background(), &client, &m)
 	if ok := assert.NoError(t, err); !ok {
-		t.FailNow()
+		t.Fail()
 	}
-	if ok := assert.Greater(t, len(matchResp), 0); !ok {
-		t.FailNow()
-	}
-	for _, m := range matchResp {
-		resp, err := fifa.GetMatchEvents(context.Background(), &client, &m)
-		if ok := assert.NoError(t, err); !ok {
-			t.Fail()
+	for _, e := range resp.NewEvents {
+		msg := fifa.ProcessEvent(context.Background(), e, &m)
+		if len(msg) == 0 {
+			continue
 		}
-		for _, e := range resp.NewEvents {
-			msg := fifa.ProcessEvent(context.Background(), e, &m)
-			if len(msg) == 0 {
-				continue
-			}
-			t.Log(msg)
-		}
+		t.Log(msg)
 	}
 }
